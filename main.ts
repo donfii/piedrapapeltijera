c4d209ce219bd21832176e6ef434b622cb072440
donfii <130453085+donfii@users.noreply.github.com>
2023-04-24 09:39:00 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+let tool = 0
+input.onGesture(Gesture.Shake, function () {
+    tool = randint(0, 2)
+    if (tool == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (tool == 1) {
+        basic.showIcon(IconNames.Scissors)
+    } else {
+        basic.showIcon(IconNames.Square)
+    }
+})
+basic.forever(function () {
+	
+})
