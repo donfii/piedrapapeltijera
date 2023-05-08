f734172291ac1a5141badd8823485405162c83e8
donfii <130453085+donfii@users.noreply.github.com>
2023-05-08 09:22:46 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,5 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (tool == 0) {
-        basic.showIcon(IconNames.Square)
-    } else if (tool == 1) {
-        basic.showIcon(IconNames.Scissors)
-    } else {
-        basic.showIcon(IconNames.SmallSquare)
-    }
+input.onButtonPressed(Button.A, function () {
+    tool = 0
 })
 input.onGesture(Gesture.Shake, function () {
     tool = randint(0, 2)
@@ -16,6 +10,21 @@ input.onGesture(Gesture.Shake, function () {
     } else {
         basic.showIcon(IconNames.SmallSquare)
     }
+})
+input.onButtonPressed(Button.AB, function () {
+    tool = 2
+})
+radio.onReceivedString(function (receivedString) {
+    if (tool == 0) {
+        basic.showIcon(IconNames.Square)
+    } else if (tool == 1) {
+        basic.showIcon(IconNames.Scissors)
+    } else {
+        basic.showIcon(IconNames.SmallSquare)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    tool = 1
 })
 let tool = 0
 radio.setGroup(69)
