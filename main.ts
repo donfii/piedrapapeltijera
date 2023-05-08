78241cf34a44b2df9bce0aa29492ab57156cc2d8
donfii <130453085+donfii@users.noreply.github.com>
2023-05-08 09:29:12 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,15 +7,6 @@ input.onGesture(Gesture.Shake, function () {
 input.onButtonPressed(Button.AB, function () {
     tool = 2
 })
-radio.onReceivedString(function (receivedString) {
-    if (tool == 0) {
-        basic.showIcon(IconNames.Square)
-    } else if (tool == 1) {
-        basic.showIcon(IconNames.Scissors)
-    } else {
-        basic.showIcon(IconNames.SmallSquare)
-    }
-})
 input.onButtonPressed(Button.B, function () {
     tool = 1
 })
